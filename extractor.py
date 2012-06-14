1de74ed43eb53b849023e0b0a3255e86c42fb9d6
Arad <ashaiber@gmail.com>
2012-06-14 17:52:43 +0300

    01.02: catch coordinates from mouse and print them

--- extractor.py
+++ extractor.py
@@ -11,29 +11,52 @@ import argparse
 
 IMAGE_FILES_ARG = 'image files'
 
-def choose_region(img):
-    cv2.imshow('image', img)
+class Extractor(object):
+    _mouse_down = False
+    _last_coords = None
 
-    while(True):
-        ch = cv2.waitKey()
-        if ch == 27:
-            break
+    def __init__(self):
+        pass
+
+    def __handle_mouse(self, event, x, y, flags, param):
+        if event == cv2.EVENT_LBUTTONDOWN:
+            self._mouse_down = True
+            self._last_coords = (x, y)
+            print("click at ({0}, {1})".format(x, y))
+        elif event == cv2.EVENT_LBUTTONUP:
+            self._mouse_down = False
+        elif event == cv2.EVENT_MOUSEMOVE and self._mouse_down:
+            self._last_coords = (x, y)
+            print("click at ({0}, {1})".format(x, y))
+
+    def __choose_region(self, img):
+        cv2.imshow('image', img)
+        cv2.setMouseCallback('image', self.__handle_mouse)
+
+        while(True):
+            ch = cv2.waitKey()
+            if ch == 27:
+                break
+        return self._last_coords
+
+    def process_images(self, files):
+        # Use first image to get user's input
+        img = cv2.imread(files[0])
+        coords = self.__choose_region(img)
 
 
-def process_images():
+if __name__ == '__main__':
+    ext = Extractor()
+
     parser = argparse.ArgumentParser(
                         description='Extract similar regions from' + 
                                      'a sequence of images')
     parser.add_argument(IMAGE_FILES_ARG, nargs='+')
     args = vars(parser.parse_args())
-
     files = args[IMAGE_FILES_ARG]
 
-    # Use first image to get user's input
-    img = cv2.imread(files[0])
-    coords, size = choose_region(img)
+    ext.process_images(files)
 
 
 
-if __name__ == '__main__':
-    process_images()
+
